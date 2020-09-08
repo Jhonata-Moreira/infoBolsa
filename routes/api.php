b579a21724c1a534faf6301e7b3663dbb206c18c
Jhonata Moreira <jhonata@idealtrends.com.br>
2020-09-08 15:20:32 -0300

    Add Stock ClientAPI

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['namespace' => 'Api', 'as' => 'api.'], function (){
+   Route::any('/Invest', 'InvestApiController@getData')->name('invest.getData');
 });
